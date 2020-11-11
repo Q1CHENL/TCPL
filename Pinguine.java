8605e79b5abf7ffd16ce540c93ed8197544a6434
Qichen Liu <liuqichne@gmail.com>
2020-11-11 21:50:18 +0100

    gut

---
+++ Pinguine.java
@@ -0,0 +1,13 @@
+package pgdp.intro;
+import static pgdp.MiniJava.write;
+
+public class Pinguine{
+    public static void main(String[] args){
+
+
+        write("Ich liebe Pinguine!");
+        write( "Wir würden gerne in die Antarktis ziehen!");
+
+    }
+
+}
